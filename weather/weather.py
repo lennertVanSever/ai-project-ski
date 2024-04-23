7df2166cec477958a08acd6233cc48528c8b9803
Lennert Van Sever <lennert.van-sever@weareretail.ai>
2024-04-23 15:25:59 +0200

    try again but doesn't work

--- weather/weather.py
+++ weather/weather.py
@@ -9,13 +9,30 @@ classes = ['misty', 'clear']
 # Define the paths to save configuration files
 data_yaml_path = os.path.join(dataset_dir, 'data.yaml')
 model_yaml_path = os.path.join(dataset_dir, 'model.yaml')
+data_yaml_content = os.path.join(dataset_dir, 'content.yaml')
+
+nc = len(classes)
+
 
 # Prepare the data.yaml file
-data_yaml_content = {
-    'train': os.path.join(dataset_dir, 'train/images'),
-    'val': os.path.join(dataset_dir, 'val/images'),
-    'nc': len(classes),
-    'names': classes
+model_yaml_content = {
+    'nc': nc,
+    'depth_multiple': 0.33,  # model depth multiple
+    'width_multiple': 0.50,  # layer channel multiple
+    # Example backbone - You need to replace this with actual YOLOv8 structure
+    'backbone': [
+        # [from, number, module, args]
+        [-1, 1, 'Conv', [32, 3, 1]],
+        [-1, 1, 'C3', [64, 3, 2]],
+        # ... more layers ...
+    ],
+    # Example head - You need to replace this with actual YOLOv8 structure
+    'head': [
+        # [from, number, module, args]
+        [-1, 3, 'Conv', [128, 3, 1]],
+        [-1, 1, 'Detect', [nc, 'anchors']],
+        # ... more layers ...
+    ],
 }
 
 # Write data configuration to yaml file
@@ -35,7 +52,7 @@ with open(model_yaml_path, 'w') as file:
     yaml.dump(model_yaml_content, file, sort_keys=False)
 
 # Initialize YOLO model for training
-model = YOLO(model_yaml_path)
+model = YOLO(model_yaml_path, task='detect')
 
 # Start training
 model.train(data_yaml_path)
