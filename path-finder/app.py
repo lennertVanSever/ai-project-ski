448b077a9f36b8860f13a07ad8fc7f7a0ea4f72c
Lennert <lennert_vansever@hotmail.be>
2024-06-07 13:41:13 +0200

    higher impact weights

--- path-finder/app.py
+++ path-finder/app.py
@@ -8,23 +8,21 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-# http://0.0.0.0:5001/graph?weather_weight=2.0&difficulty_weight=1.5&waiting_time_weight=1.0
+
 @app.route('/graph')
 def graph():
-    # Get weights from URL parameters or use defaults
     weather_weight = float(request.args.get('weather_weight', 2.0))
     difficulty_weight = float(request.args.get('difficulty_weight', 1.5))
     waiting_time_weight = float(request.args.get('waiting_time_weight', 1.0))
-    
+
     weights = {
         'weather_weight': weather_weight,
         'difficulty_weight': difficulty_weight,
         'waiting_time_weight': waiting_time_weight
     }
-    
-    # Create the ski resort graph with the given weights
+
     ski_resort = create_ski_resort(weights)
-    pos = nx.spring_layout(ski_resort)  # Layout for the node positions
+    pos = nx.spring_layout(ski_resort)
 
     elements = {'nodes': [], 'edges': []}
     for node in ski_resort.nodes():
@@ -45,29 +43,27 @@ def graph():
 
     return jsonify(elements)
 
+
 @app.route('/find_path')
 def find_path():
-    # Get weights from URL parameters or use defaults
     weather_weight = float(request.args.get('weather_weight', 2.0))
     difficulty_weight = float(request.args.get('difficulty_weight', 1.5))
     waiting_time_weight = float(request.args.get('waiting_time_weight', 1.0))
-    
+
     weights = {
         'weather_weight': weather_weight,
         'difficulty_weight': difficulty_weight,
         'waiting_time_weight': waiting_time_weight
     }
-    
-    # Create the ski resort graph with the given weights each time a request is made
+
     ski_resort = create_ski_resort(weights)
-    
-    # Get the start and end nodes along with the desired time from URL parameters
+
     start = request.args.get('start')
     end = request.args.get('end')
     desired_time_minutes = int(request.args.get('time', 0))
-    
-    # Find path using the provided utility function
-    path, _ = find_initial_path_with_time_constraint(ski_resort, start, end, desired_time_minutes)
+
+    path, _ = find_initial_path_with_time_constraint(
+        ski_resort, start, end, desired_time_minutes)
     return jsonify({'path': path})
 
 
--- path-finder/graph_utils.py
+++ path-finder/graph_utils.py
@@ -1,36 +1,49 @@
 import networkx as nx
 
+
 def add_slope(graph, start, end, weather, difficulty, average_time, weights):
-    weight = weights['weather_weight'] * weather + weights['difficulty_weight'] * (1 - difficulty)
-    graph.add_edge(start, end, weight=weight, type='slope', weather=weather, difficulty=difficulty, time=average_time)
+    weight = weights['weather_weight'] * weather + \
+        weights['difficulty_weight'] * (1 - difficulty)
+    graph.add_edge(start, end, weight=weight, type='slope',
+                   weather=weather, difficulty=difficulty, time=average_time)
+
 
 def add_lift(graph, start, end, waiting_time, average_time, weights):
     weight = weights['waiting_time_weight'] * waiting_time
-    graph.add_edge(start, end, weight=weight, type='lift', waiting_time=waiting_time, time=average_time)
+    graph.add_edge(start, end, weight=weight, type='lift',
+                   waiting_time=waiting_time, time=average_time)
+
 
 def find_initial_path_with_time_constraint(graph, start, end, desired_time_minutes):
     all_paths = list(nx.all_simple_paths(graph, start, end))
     closest_path = None
-    closest_time_diff = float('inf')
+    closest_weighted_time_diff = float('inf')
 
     for path in all_paths:
-        total_time = sum(graph[path[i]][path[i+1]]['time'] for i in range(len(path)-1))
-        time_diff = abs(total_time - desired_time_minutes)
+        total_time = sum(graph[path[i]][path[i+1]]['time']
+                         for i in range(len(path)-1))
+        total_weight = sum(graph[path[i]][path[i+1]]['weight']
+                           for i in range(len(path)-1))
+        weighted_time = total_time * total_weight
+        weighted_time_diff = abs(weighted_time - desired_time_minutes)
 
-        if time_diff < closest_time_diff:
+        if weighted_time_diff < closest_weighted_time_diff:
             closest_path = path
-            closest_time_diff = time_diff
+            closest_weighted_time_diff = weighted_time_diff
 
-    return closest_path, closest_time_diff
+    return closest_path, closest_weighted_time_diff
+
 
 def extend_path_if_needed(graph, path, desired_time_minutes):
-    total_time = sum(graph[path[i]][path[i+1]]['time'] for i in range(len(path)-1))
+    total_time = sum(graph[path[i]][path[i+1]]['time']
+                     for i in range(len(path)-1))
     extended_path = list(path)
 
     for i in range(1, len(path)-2):
         for neighbor in graph.neighbors(path[i]):
             if neighbor not in path and graph.has_edge(neighbor, path[i+1]):
-                detour_time = graph[path[i]][neighbor]['time'] + graph[neighbor][path[i+1]]['time']
+                detour_time = graph[path[i]][neighbor]['time'] + \
+                    graph[neighbor][path[i+1]]['time']
                 if total_time + detour_time <= desired_time_minutes:
                     extended_path.insert(i+1, neighbor)
                     total_time += detour_time
